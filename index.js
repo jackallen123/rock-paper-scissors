639b505fc66a50789917dcde267ae6920f02c070
jackallen123 <128607553+jackallen123@users.noreply.github.com>
2023-05-10 12:42:27 -0400

    Add function game that plays 5 rounds of RPS

--- index.js
+++ index.js
@@ -30,26 +30,68 @@ function playGame(playerSelection, computerSelection) {
 
     // draw cases
     if (a == computerSelection) {
-        return 'The game is a draw!';
+        return 'Draw';
     }
 
     // win cases
     else if ((a == 'Rock' && computerSelection == 'Scissors') ||
             (a == 'Scissors' && computerSelection == 'Paper') ||
             (a == 'Paper' && computerSelection == 'Rock')) {
-                return ('You win! ' + a + ' beats ' + computerSelection + '.');
+                return 'Win';
     }
     
     // if it isn't a draw and you don't win, you must lose
     else {
-        return ('You lose! ' + computerSelection + ' beats ' + a + '.');
+        return 'Lose';
     }
 }
 
-// get user input
-let playerSelection = prompt("Enter Rock, Paper or Scissors: \n");
-// generate computer choice
-let computerSelection = getComputerChoice();
+/*
+    Plays a 5 round game that keeps score and reports a winner or loser at the end
+*/
 
-// print results
-console.log(playGame(playerSelection, computerSelection))
+function game() {
+    // variables to keep track of score
+    let draws = 0;
+    let wins = 0;
+    let losses = 0;
+
+    // play the game 5 times
+    for (let i = 0; i < 5; i++) {
+        // get input from user
+        let playerSelection = prompt("Enter rock, paper, or scissors to play the game: \n");
+
+        // generate computer's play
+        let computerSelection = getComputerChoice();
+
+        // get result from this round
+        let result = playGame(playerSelection, computerSelection);
+
+        // if the game is a draw
+        if (result == 'Draw') {
+            // print result to console
+            console.log("This round is a draw!")
+            // increment draw counter
+            draws++;
+        }
+
+        // if user wins the round
+        else if (result == 'Win') {
+            // print result to console
+            console.log("You win this round!" + playerSelection + " beats " + computerSelection + ".")
+            // icrement wins counter
+            wins++;
+        }
+
+        // if user loses the round
+        else {
+            // print result to console
+            console.log("You lose this round! " + computerSelection + " beats " + playerSelection + ".")
+            // increment losses counter
+            losses++;
+        }
+
+        // at the end of round, print updated score
+        console.log("(W/L/D) Current score is: " + wins + " - " + losses + " - " + draws)
+    }
+}
